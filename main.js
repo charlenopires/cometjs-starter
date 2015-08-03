02c095e841d41732f236d016b173a3535a93bbb1
Charleno Pires <charleno@gmail.com>
2015-08-03 20:05:39 -0300

    Changing for Stylus

--- main.js
+++
@@ -1,4 +0,0 @@
-Meteor.startup (function () {
-  // Example: WebApp.addHtmlAttributeHook( function (request) { return {'lang' : 'pt-BR', 'dir' : 'ltr'} });
-  WebApp.addHtmlAttributeHook( function (request) { return {'lang' : '', 'dir' : ''} });
-});
